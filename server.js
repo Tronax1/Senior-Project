bb17e596863dda7d28042425721254f452d628e3
Gongo <jvilla1714@gmail.com>
2019-09-24 16:55:44 -0400

    Setup MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoDBRoute = process.env.DATA_BASE_ROUTE;
+
+mongoose.connect(mongoDBRoute, { useNewUrlParser: true});
+
+let db = mongoose.connection;
+
+db.once('open', ()=> console.log('Connected to the database'));
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const app = express();
 
