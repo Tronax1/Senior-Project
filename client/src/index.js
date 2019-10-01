92c7d5950687df9e65a45dc272c0d5e1e8bed2dd
Gongo <jvilla1714@gmail.com>
2019-09-30 20:46:40 -0400

    Added redux to the project

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import { reduxThunk } from 'redux-thunk';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-<BrowserRouter>
-    <App />
-</BrowserRouter>, document.getElementById('root'));
+<Provider store={store}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
