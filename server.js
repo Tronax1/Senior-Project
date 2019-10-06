d6abcce984ac47caf4cc26ae05f3942793fd3980
Mathew <mathewk@mail.usf.edu>
2019-10-06 18:21:26 -0400

    upload data to server

--- server.js
+++ server.js
@@ -5,13 +5,19 @@ require('dotenv').config();
 const mongoDBRoute = process.env.DATA_BASE_ROUTE;
 
 mongoose.connect(mongoDBRoute, { useNewUrlParser: true});
-
+console.log('route:',mongoDBRoute)
 let db = mongoose.connection;
 
 db.once('open', ()=> console.log('Connected to the database'));
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+var TicketData = require('./TicketData.json');
+
+const Tickets= db.collection('TicketInfo')
+Tickets.insertMany(TicketData, (err,result)=>{
+    console.log('Ticket Data stored')
+})
 const app = express();
 
 const PORT = 5000;
