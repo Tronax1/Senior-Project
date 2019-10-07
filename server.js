d6701571957acd8860c5ac76fdcd27e7fdca8ece
Gongo <jvilla1714@gmail.com>
2019-10-07 19:15:03 -0400

    Added Schema for the Ticket

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const Ticket = require('./Model/TicketSchema');
 
 const mongoDBRoute = process.env.DATA_BASE_ROUTE;
 
@@ -12,6 +13,11 @@ db.once('open', ()=> console.log('Connected to the database'));
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+getData(Ticket);
+async function getData(Ticket){
+    const data = await Ticket.find({OID: 1});
+    console.log(data);
+}
 const app = express();
 
 const PORT = 5000;
