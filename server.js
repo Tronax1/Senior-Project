33795412d804ebcdc39ba17332852ff3ee01fb0f
Gongo <jvilla1714@gmail.com>
2019-10-12 15:19:49 -0400

    Fixed different file name

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ app.use('/api/items', items);
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('my-app/build'));
+    app.use(express.static('client/build'));
 
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'my-app', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
