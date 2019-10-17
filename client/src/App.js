c4ab24cf665ee309bd3e46acfccd14ae09865ad4
Tronax1 <jvilla1714@gmail.com>
2019-10-17 14:33:01 -0400

    Added private routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import Login from './Components/Login'
 import TicketPage from './Components/TicketPage'
 import NavBar from './Components/NavBar/NavBar'
@@ -12,6 +12,16 @@ import { fetchUser } from './actions'
 function NoPage(){
   return <h1 style={{textAlign: "center"}}>404 Not Found</h1>;
 }
+
+function PrivateRoute(props){
+    if(props.user === null){
+      return <Redirect to="/" exact/>
+    }
+    else{
+      return <Route path={props.path} component={props.item}/>
+    }
+}
+
 class App extends Component {
   componentDidMount(){
     this.props.fetchUser();
@@ -22,14 +32,16 @@ class App extends Component {
         <NavBar />
         <Switch>
           <Route path="/" component={Login} exact />
-          <Route path="/User/Tickets" component={TicketPage} />
-          <Route path="/User/TableSelection" component={TableCheckbox} />
-          <Route path="/User/Results" component={CompResults}/>
-          <Route component={NoPage} />
+          <PrivateRoute user={this.props.user} path="/User/Tickets" item={TicketPage}/>
+          <PrivateRoute user={this.props.user} path="/User/TableSelection" item={TableCheckbox}/>
+          <PrivateRoute user={this.props.user} path="/User/Results" item={CompResults}/>
+          <Route component={NoPage}/>
         </Switch>
       </>
     );
   }
 }
-
-export default connect(null, {fetchUser})(App);
+function mapStatetoProps({user}){
+  return {user};
+}
+export default connect(mapStatetoProps, {fetchUser})(App);
